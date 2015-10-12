814867f27413f582a429ce4b5898e9c17b691983
Dawid Aniol <Dawid.Aniol@grupaonet.pl>
2015-10-12 18:10:47 +0200

    clean file tree

--- scripts/build_deb.sh
+++ scripts/build_deb.sh
@@ -1,17 +1,16 @@
 #! /usr/bin/env bash
 
+echo "Prepare..."
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-
 cd ./../
 rm -f ./deb/usr/local/bin/clipboard.py
 rm -f ./deb/usr/share/cicero/ajt.db
 cp ./clipboard.py ./deb/usr/local/bin/clipboard.py
 cp ./ajt.db ./deb/usr/share/cicero/ajt.db
 
-cd deb
-
 echo "Build package..."
+cd deb
 fakeroot dpkg-deb --build .
 rm -f ./../cicero_ang_pol_slownik.deb
 mv ./..deb ./../cicero_ang_pol_slownik.deb
