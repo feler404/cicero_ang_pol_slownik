feeb3b82822167141cd9d5d3f5ada762c2a7543d
Dawid Aniol <Dawid.Aniol@grupaonet.pl>
2015-11-05 12:25:05 +0100

    update tests

--- scripts/test_on_desktop.sh
+++ scripts/test_on_desktop.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
 # download and install
-rm -f cicer_ang_pol_slownik.dev
-wget https://github.com/feler404/cicero_ang_pol_slownik/blob/master/build/cicero_ang_pol_slownik.deb
-sudo dpkg -i ./cicer_ang_pol_slownik.deb
+echo "Prepare..."
+rm -f /tmp/cicero_ang_pol_slownik.deb
+wget "https://github.com/feler404/cicero_ang_pol_slownik/blob/master/build/cicero_ang_pol_slownik.deb?raw=true" -P /tmp/
+mv /tmp/cicero_ang_pol_slownik.deb?raw=true /tmp/cicero_ang_pol_slownik.deb
+sudo dpkg -i /tmp/cicero_ang_pol_slownik.deb
+
 
 # run and wait on exit
+echo "Run..."
 python /usr/local/bin/clipboard.py
 
 # cleanup
-sudo apt-get remove -y cicer_ang_pol_slownik
-rm -f cicer_ang_pol_slownik.deb
+echo "Cleanup..."
+sudo apt-get remove -y cicero-ang-pol-slownik
+rm -f /tmp/cicero_ang_pol_slownik.deb
+
+echo "Done."
